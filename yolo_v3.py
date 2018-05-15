eba7cf6ac351b007e55ba8f30e07bff52a55baea
Paweł Kapica <pawel.kapica@gmail.com>
2018-05-15 15:52:18 +0200

    set slim arg scope params

--- yolo_v3.py
+++ yolo_v3.py
@@ -4,6 +4,10 @@ import tensorflow as tf
 
 slim = tf.contrib.slim
 
+_BATCH_NORM_DECAY = 0.9
+_BATCH_NORM_EPSILON = 1e-05
+_LEAKY_RELU = 0.1
+
 
 def darknet53(inputs):
     """
@@ -16,15 +20,38 @@ def yolo_v3(inputs, num_classes, is_training=False, data_format='NCHW', reuse=Fa
     """
     Creates YOLO v3 model.
 
-    :param inputs: a 4-D tensor of size [batch_size, height, width, channels] or [batch_size, channels, height, width].
+    :param inputs: a 4-D tensor of size [batch_size, height, width, channels].
         Dimension batch_size may be undefined.
     :param num_classes: number of predicted classes.
     :param is_training: whether is training or not.
     :param data_format: data format NCHW or NHWC.
     :param reuse: whether or not the network and its variables should be reused.
-    :return: 
+    :return:
     """
-    pass
+    # transpose the inputs to NCHW
+    if data_format == 'NCHW':
+        inputs = tf.transpose(inputs, [0, 3, 1, 2])
+
+    # normalize values to range [0..1]
+    inputs = inputs / 255
+
+    # set batch norm params
+    batch_norm_params = {
+        'decay': _BATCH_NORM_DECAY,
+        'epsilon': _BATCH_NORM_EPSILON,
+        'scale': True,
+        'is_training': is_training,
+        'fused': None,  # Use fused batch norm if possible.
+    }
+
+    # Set activation_fn and parameters for conv2d, batch_norm.
+    with slim.arg_scope([slim.conv2d, slim.batch_norm], activation_fn=lambda x: tf.nn.leaky_relu(x, alpha=_LEAKY_RELU), data_format=data_format,
+                        reuse=reuse):
+        with slim.arg_scope([slim.conv2d], normalizer_fn=slim.batch_norm, normalizer_params=batch_norm_params,
+                            biases_initializer=None):
+            with tf.variable_scope('darknet-53'):
+                inputs = darknet53(inputs)
+
 
 
 def load_weights(var_list, weights_file):
