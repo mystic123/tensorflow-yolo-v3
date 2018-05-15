bc1f100d66dd59176e00c409e3b82ab1d1692a2a
Paweł Kapica <pawel.kapica@gmail.com>
2018-05-15 16:40:41 +0200

    darknet-53 model

--- yolo_v3.py
+++ yolo_v3.py
@@ -13,7 +13,76 @@ def darknet53(inputs):
     """
     Builds Darknet-53 model.
     """
-    pass
+    inputs = _conv2d_fixed_padding(inputs, 32, 3)
+    inputs = _conv2d_fixed_padding(inputs, 64, 3, strides=2)
+    inputs = _darknet53_block(inputs, 32)
+    inputs = _conv2d_fixed_padding(inputs, 128, 3, strides=2)
+
+    for i in range(2):
+        inputs = _darknet53_block(inputs, 64)
+
+    inputs = _conv2d_fixed_padding(inputs, 256, 3, strides=2)
+
+    for i in range(8):
+        inputs = _darknet53_block(inputs, 128)
+
+    inputs = _conv2d_fixed_padding(inputs, 512, 3, strides=2)
+
+    for i in range(8):
+        inputs = _darknet53_block(inputs, 256)
+
+    inputs = _conv2d_fixed_padding(inputs, 1024, 3, strides=2)
+
+    for i in range(4):
+        inputs = _darknet53_block(inputs, 512)
+
+    return inputs
+
+
+def _conv2d_fixed_padding(inputs, filters, kernel_size, strides=1):
+    if strides > 1:
+        inputs = _fixed_padding(inputs, kernel_size)
+    inputs = slim.conv2d(inputs, filters, kernel_size)
+    return inputs
+
+
+def _darknet53_block(inputs, filters):
+    shortcut = inputs
+    inputs = _conv2d_fixed_padding(inputs, filters, 1)
+    inputs = _conv2d_fixed_padding(inputs, filters * 2, 3)
+
+    inputs = inputs + shortcut
+    return inputs
+
+
+@tf.contrib.framework.add_arg_scope
+def _fixed_padding(inputs, kernel_size, *args, mode='CONSTANT', **kwargs):
+    """
+    Pads the input along the spatial dimensions independently of input size.
+
+    Args:
+      inputs: A tensor of size [batch, channels, height_in, width_in] or
+        [batch, height_in, width_in, channels] depending on data_format.
+      kernel_size: The kernel to be used in the conv2d or max_pool2d operation.
+                   Should be a positive integer.
+      data_format: The input format ('NHWC' or 'NCHW').
+      mode: The mode for tf.pad.
+
+    Returns:
+      A tensor with the same format as the input with the data either intact
+      (if kernel_size == 1) or padded (if kernel_size > 1).
+    """
+    pad_total = kernel_size - 1
+    pad_beg = pad_total // 2
+    pad_end = pad_total - pad_beg
+
+    if kwargs['data_format'] == 'NCHW':
+        padded_inputs = tf.pad(inputs, [[0, 0], [0, 0],
+                                        [pad_beg, pad_end], [pad_beg, pad_end]], mode=mode)
+    else:
+        padded_inputs = tf.pad(inputs, [[0, 0], [pad_beg, pad_end],
+                                        [pad_beg, pad_end], [0, 0]], mode=mode)
+    return padded_inputs
 
 
 def yolo_v3(inputs, num_classes, is_training=False, data_format='NCHW', reuse=False):
@@ -45,7 +114,8 @@ def yolo_v3(inputs, num_classes, is_training=False, data_format='NCHW', reuse=Fa
     }
 
     # Set activation_fn and parameters for conv2d, batch_norm.
-    with slim.arg_scope([slim.conv2d, slim.batch_norm], activation_fn=lambda x: tf.nn.leaky_relu(x, alpha=_LEAKY_RELU),
+    with slim.arg_scope([slim.conv2d, slim.batch_norm, _fixed_padding],
+                        activation_fn=lambda x: tf.nn.leaky_relu(x, alpha=_LEAKY_RELU),
                         data_format=data_format, reuse=reuse):
         with slim.arg_scope([slim.conv2d], normalizer_fn=slim.batch_norm, normalizer_params=batch_norm_params,
                             biases_initializer=None):
