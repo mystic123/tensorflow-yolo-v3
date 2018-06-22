1d67c649e0855dfdf48c578267777dc29ababd57
mystic123 <pawel.kapica@gmail.com>
2018-06-22 16:19:43 +0200

    add max_pool2d data_format arg to scope

--- yolo_v3_tiny.py
+++ yolo_v3_tiny.py
@@ -44,43 +44,44 @@ def yolo_v3_tiny(inputs, num_classes, is_training=False, data_format='NCHW', reu
     }
 
     # Set activation_fn and parameters for conv2d, batch_norm.
-    with slim.arg_scope([slim.conv2d, slim.batch_norm, _fixed_padding], data_format=data_format, reuse=reuse):
-        with slim.arg_scope([slim.conv2d], 
-                        normalizer_fn=slim.batch_norm, 
-                        normalizer_params=batch_norm_params,
-                        biases_initializer=None, 
-                        activation_fn=lambda x: tf.nn.leaky_relu(x, alpha=_LEAKY_RELU)):
+    with slim.arg_scope([slim.conv2d, slim.batch_norm, _fixed_padding, slim.max_pool2d], data_format=data_format):
+        with slim.arg_scope([slim.conv2d, slim.batch_norm, _fixed_padding], reuse=reuse):
+            with slim.arg_scope([slim.conv2d], 
+                            normalizer_fn=slim.batch_norm, 
+                            normalizer_params=batch_norm_params,
+                            biases_initializer=None, 
+                            activation_fn=lambda x: tf.nn.leaky_relu(x, alpha=_LEAKY_RELU)):
 
-            with tf.variable_scope('yolo-v3-tiny'):
-                for i in range(6):
-                    inputs = _conv2d_fixed_padding(inputs, 16 * pow(2, i), 3)
+                with tf.variable_scope('yolo-v3-tiny'):
+                    for i in range(6):
+                        inputs = _conv2d_fixed_padding(inputs, 16 * pow(2, i), 3)
 
-                    if i == 4:
-                        route_1 = inputs
+                        if i == 4:
+                            route_1 = inputs
 
-                    inputs = slim.max_pool2d(inputs, [2, 2], scope='pool2')
-                
-                inputs = _conv2d_fixed_padding(inputs, 1024, 3)
-                inputs = _conv2d_fixed_padding(inputs, 256, 1)
-                route_2 = inputs
+                        inputs = slim.max_pool2d(inputs, [2, 2], scope='pool2')
+                    
+                    inputs = _conv2d_fixed_padding(inputs, 1024, 3)
+                    inputs = _conv2d_fixed_padding(inputs, 256, 1)
+                    route_2 = inputs
 
-                inputs = _conv2d_fixed_padding(inputs, 512, 3)
-                # inputs = _conv2d_fixed_padding(inputs, 255, 1)
+                    inputs = _conv2d_fixed_padding(inputs, 512, 3)
+                    # inputs = _conv2d_fixed_padding(inputs, 255, 1)
 
-                detect_1 = _detection_layer(inputs, num_classes, _ANCHORS[3:6], img_size, data_format)
-                detect_1 = tf.identity(detect_1, name='detect_1')
+                    detect_1 = _detection_layer(inputs, num_classes, _ANCHORS[3:6], img_size, data_format)
+                    detect_1 = tf.identity(detect_1, name='detect_1')
 
-                inputs = _conv2d_fixed_padding(route_2, 128, 1)
-                upsample_size = route_1.get_shape().as_list()
-                inputs = _upsample(inputs, upsample_size, data_format)
+                    inputs = _conv2d_fixed_padding(route_2, 128, 1)
+                    upsample_size = route_1.get_shape().as_list()
+                    inputs = _upsample(inputs, upsample_size, data_format)
 
-                inputs = tf.concat([inputs, route_1], axis=1 if data_format == 'NCHW' else 3)
+                    inputs = tf.concat([inputs, route_1], axis=1 if data_format == 'NCHW' else 3)
 
-                inputs = _conv2d_fixed_padding(inputs, 256, 3)
-                # inputs = _conv2d_fixed_padding(inputs, 255, 1)
+                    inputs = _conv2d_fixed_padding(inputs, 256, 3)
+                    # inputs = _conv2d_fixed_padding(inputs, 255, 1)
 
-                detect_2 = _detection_layer(inputs, num_classes, _ANCHORS[0:3], img_size, data_format)
-                detect_2 = tf.identity(detect_2, name='detect_2')
+                    detect_2 = _detection_layer(inputs, num_classes, _ANCHORS[0:3], img_size, data_format)
+                    detect_2 = tf.identity(detect_2, name='detect_2')
 
-                detections = tf.concat([detect_1, detect_2], axis=1)
-                return detections
+                    detections = tf.concat([detect_1, detect_2], axis=1)
+                    return detections
