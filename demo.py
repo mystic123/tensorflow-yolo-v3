57d1cafd36f7fdfeb56b80ed675d396559cf7f5c
LucasMahieu <lmahieu@kalray.eu>
2018-10-22 14:47:29 +0200

    Improve demo.py script to support yolov3 AND tiny-yolov3
    Also add the possibility to save trained values and graph model

--- demo.py
+++ demo.py
@@ -5,18 +5,25 @@ import tensorflow as tf
 from PIL import Image, ImageDraw
 
 from yolo_v3 import yolo_v3, load_weights, detections_boxes, non_max_suppression
+from yolo_v3_tiny import yolo_v3_tiny
 
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('input_img', '', 'Input image')
 tf.app.flags.DEFINE_string('output_img', '', 'Output image')
-tf.app.flags.DEFINE_string('class_names', 'coco.names', 'File with class names')
+tf.app.flags.DEFINE_string('class_names', 'classes.txt', 'File with class names')
 tf.app.flags.DEFINE_string('weights_file', 'yolov3.weights', 'Binary file with detector weights')
+tf.app.flags.DEFINE_string('tiny_weights_file', 'yolov3-tiny.weights', 'Binary file with detector weights for tiny model')
 
 tf.app.flags.DEFINE_integer('size', 416, 'Image size')
 
 tf.app.flags.DEFINE_float('conf_threshold', 0.5, 'Confidence threshold')
 tf.app.flags.DEFINE_float('iou_threshold', 0.4, 'IoU threshold')
+
+tf.app.flags.DEFINE_bool('is_tiny', False, 'set to True to use the tiny model')
+
+tf.app.flags.DEFINE_bool('enable_saver', True, 'set to True to save the model with trained data')
+tf.app.flags.DEFINE_bool('write_model_graph', True, 'set to True to save the graph of the model in a graph.pb file')
 
 
 def load_coco_names(file_name):
@@ -39,7 +46,7 @@ def draw_boxes(boxes, img, cls_names, detection_size):
 
 
 def convert_to_original_size(box, size, original_size):
-    ratio = original_size / size
+    ratio = np.float32(original_size) / np.float32(size)
     box = box.reshape(2, 2) * ratio
     return list(box.reshape(-1))
 
@@ -51,18 +58,31 @@ def main(argv=None):
     classes = load_coco_names(FLAGS.class_names)
 
     # placeholder for detector inputs
-    inputs = tf.placeholder(tf.float32, [None, FLAGS.size, FLAGS.size, 3])
+    inputs = tf.placeholder(tf.float32, [1, FLAGS.size, FLAGS.size, 3])
 
     with tf.variable_scope('detector'):
-        detections = yolo_v3(inputs, len(classes), data_format='NCHW')
-        load_ops = load_weights(tf.global_variables(scope='detector'), FLAGS.weights_file)
+        if FLAGS.is_tiny:
+            detections = yolo_v3_tiny(inputs, len(classes), data_format='NHWC')
+            load_ops = load_weights(tf.global_variables(scope='detector'), FLAGS.tiny_weights_file)
+        else:
+            detections = yolo_v3(inputs, len(classes), data_format='NHWC')
+            load_ops = load_weights(tf.global_variables(scope='detector'), FLAGS.weights_file)
 
     boxes = detections_boxes(detections)
 
-    with tf.Session() as sess:
-        sess.run(load_ops)
+    if FLAGS.enable_saver:
+        saver = tf.train.Saver()
 
+    with tf.Session() as sess:
+        if FLAGS.write_model_graph:
+            tf.train.write_graph(sess.graph_def,'./', "graph.pb", False)
+
+        sess.run(load_ops)
         detected_boxes = sess.run(boxes, feed_dict={inputs: [np.array(img_resized, dtype=np.float32)]})
+
+        if FLAGS.enable_saver:
+            save_path = saver.save(sess, "./model.ckpt")
+            print("Model saved in path: %s" % save_path)
 
     filtered_boxes = non_max_suppression(detected_boxes, confidence_threshold=FLAGS.conf_threshold,
                                          iou_threshold=FLAGS.iou_threshold)
@@ -70,7 +90,6 @@ def main(argv=None):
     draw_boxes(filtered_boxes, img, classes, (FLAGS.size, FLAGS.size))
 
     img.save(FLAGS.output_img)
-
 
 if __name__ == '__main__':
     tf.app.run()
