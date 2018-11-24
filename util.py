d3b73738ebefd7609e5602cc56522b49017bc4d4
BoBo san <i3oi3osan@gmail.com>
2018-11-24 22:02:39 +0700

    Use Pillow module instead of OpenCV.

--- util.py
+++ util.py
@@ -1,26 +1,26 @@
 import numpy as np
-import cv2
+from PIL import Image
 
 
-def letter_box_image(image: np.ndarray, output_height: int, output_width: int, fill_value)-> np.ndarray:
+def letter_box_image(image: Image.Image, output_height: int, output_width: int, fill_value)-> np.ndarray:
     """
     Fit image with final image with output_width and output_height.
-    :param image: numpy image -> shape( height, width, channel )
+    :param image: PILLOW Image object.
     :param output_height: width of the final image.
     :param output_width: height of the final image.
-    :param fill_value: fill value for empty area. Can be single number or np.ndarray
-    :return: numpy image fit within letterbox
+    :param fill_value: fill value for empty area. Can be uint8 or np.ndarray
+    :return: numpy image fit within letterbox. dtype=uint8, shape=(output_height, output_width)
     """
 
-    height_ratio = float(output_height)/image.shape[0]
-    width_ratio = float(output_width)/image.shape[1]
+    height_ratio = float(output_height)/image.size[1]
+    width_ratio = float(output_width)/image.size[0]
     fit_ratio = min(width_ratio, height_ratio)
-    fit_height = int(image.shape[0] * fit_ratio)
-    fit_width = int(image.shape[1] * fit_ratio)
-    fit_image = cv2.resize(image, (fit_width, fit_height), interpolation=cv2.INTER_LINEAR)
+    fit_height = int(image.size[1] * fit_ratio)
+    fit_width = int(image.size[0] * fit_ratio)
+    fit_image = np.asarray(image.resize((fit_width, fit_height), resample=Image.BILINEAR))
 
-    if isinstance(fill_value, float) or isinstance(fill_value, int):
-        fill_value = np.full(image.shape[2], fill_value, image.dtype)
+    if isinstance(fill_value, int):
+        fill_value = np.full(fit_image.shape[2], fill_value, fit_image.dtype)
 
     to_return = np.tile(fill_value, (output_height, output_width, 1))
     pad_top = int(0.5 * (output_height - fit_height))
